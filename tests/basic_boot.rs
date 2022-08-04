578aeb53893f9dd323edcc58437392caaff17440
Daniel Arduini <danielarduini47@gmail.com>
2022-08-04 11:12:49 +0200

    Fixed wrong directory name for test

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,20 +7,20 @@
 use core::panic::PanicInfo;
 use rust_os::println;
 
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
-}
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
 #[test_case]
